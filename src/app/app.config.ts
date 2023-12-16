04f8105dcddb6ba38e1cc6c94d7e5ca471e20073
Nicolas Molina Monroy <hi@nicobytes.com>
2023-12-16 08:11:04 -0400

    save

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from "@angular/common/http";;
+import { provideHttpClient, withFetch } from "@angular/common/http";;
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,5 +9,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient()]
+    provideHttpClient(withFetch())]
 };
