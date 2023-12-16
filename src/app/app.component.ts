bfa3439d476e277e42c8abc87cdde651d2a15ea9
Nicolas Molina Monroy <hi@nicobytes.com>
2023-12-16 08:03:44 -0400

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular--ssr';
+  http = inject(HttpClient);
+  products  = signal<any[]>([])
+  ngOnInit() {
+    this.http.get<any[]>('https://api.escuelajs.co/api/v1/products').subscribe(data => {
+      this.products.set(data);
+    });
+  }
 }
